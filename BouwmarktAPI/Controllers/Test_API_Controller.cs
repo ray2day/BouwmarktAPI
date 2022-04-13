baa3f5c214c5685837066a26523bc2be1af4d91c
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-04-13 16:48:03 +0200

    Add project files.

---
+++ BouwmarktAPI/Controllers/Test_API_Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BouwmarktAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test_API_Controller : ControllerBase
+    {
+    }
+}
---
+++ BouwmarktAPI/Vestiging.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BouwmarktAPI
+{
+    public class Vestiging
+    {
+        [Key]
+        [Display(Name = "Vestigingsnummer")]
+        public int VestigingsNummer { get; set; }
+        [Required(ErrorMessage = "Naam is verplicht")]
+        public string Naam { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Adresgegevens zijn verplicht")]
+        public string Adres { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Plaatsnaam is verplicht")]
+        public string Plaats { get; set; } = String.Empty;
+        [Display(Name = "Telefoonnummer")]
+        [Required(ErrorMessage = "Telefoonnummer is verplicht")]
+        public string TelefoonNummer { get; set; } = String.Empty;
+        }
+}
